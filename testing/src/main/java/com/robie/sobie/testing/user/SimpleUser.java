9bbc4f028a407a903b9bfb59d8a5045378932073
Pik0Pak0 <mic.toc.work@gmail.com>
2024-07-09 21:44:49 +0200

    Added realName field (SimpleUser)  and improvement of test suite

--- testing/src/main/java/com/robie/sobie/testing/user/SimpleUser.java
+++ testing/src/main/java/com/robie/sobie/testing/user/SimpleUser.java
@@ -2,11 +2,22 @@ package com.robie.sobie.testing.user;
 
 public class SimpleUser {
     private String username;
+    private String realName;
 
     public SimpleUser(String username) {
         this.username = username;
     }
+
+    public SimpleUser(String username, String realName) {
+        this.username = username;
+        this.realName = realName;
+    }
+
     public String getUsername(){
         return username;
+    }
+
+    public String getRealName(){
+        return realName;
     }
 }
--- testing/src/test/java/com/robie/sobie/testing/forum/ForumTestSuite.java
+++ testing/src/test/java/com/robie/sobie/testing/forum/ForumTestSuite.java
@@ -1,10 +1,50 @@
 package com.robie.sobie.testing.forum;
 
 import com.robie.sobie.testing.user.SimpleUser;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+@DisplayName("Forum Test Suite")
 public class ForumTestSuite {
+
+    @BeforeEach
+    public void before(){
+        System.out.println("Test Case: begin");
+    }
+
+    @AfterEach
+    public void after(){
+        System.out.println("Test Case: end");
+    }
+
+    @BeforeAll
+    public static void beforeAll(){
+        System.out.println("Test Suite: begin");
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        System.out.println("Test Suite: end");
+    }
+
+    @DisplayName("When create SimpleUser with realName, "+
+    "then getRealName should return real user name")
+
+    @Test
+    void testCaseRealName() {
+        //Given
+        SimpleUser user = new SimpleUser("TheForumUser","John Smith");
+
+        //When
+        String result = user.getRealName();
+        System.out.println("Testing " + result);
+
+        //Then
+        Assertions.assertEquals("John Smith",result);
+    }
+
+    @DisplayName("When created SimpleUser with name, " +
+    "then getUsername should return correct name")
+
     @Test
     void testCaseUsername() {
         //Given
@@ -12,7 +52,7 @@ public class ForumTestSuite {
 
         //When
         String result = user.getUsername();
-        String expectedResult = "theForumUser";
+        String expectedResult = "TheForumUser";
 
         //Then
         Assertions.assertEquals(expectedResult, result);
